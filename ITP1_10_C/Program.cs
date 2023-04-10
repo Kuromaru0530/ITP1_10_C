fe9ffcdceb48e380913a9c4c2dde4ed509551e00
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:34:08 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_10_C/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        while(true)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0) break;
+
+            List<double> s = new List<double>();
+
+            var input = Console.ReadLine().Split(' ');
+            for (int i = 0; i < input.Length; i++)
+            {
+                s.Add(int.Parse(input[i]));
+            }
+            var ave = s.Average();
+
+            List<double> diff = new List<double>();
+
+            s.ForEach(x => diff.Add(Math.Pow(x - ave, 2)));
+            var v = Math.Sqrt(diff.Sum() / n);
+            Console.WriteLine(v);
+        }
+        
+    }
+}
